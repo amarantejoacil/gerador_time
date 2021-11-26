116ae86b828e7782d8dfd10fcb735c1f7b4cb9c8
Joacil Amarante <joacil.amarante@gmail.com>
2021-11-26 19:34:31 -0400

    regerar

--- project/settings.py
+++ project/settings.py
@@ -97,11 +97,14 @@ DATABASES = {
         'PORT': '5432',
     }
 }
+
+
 """
 
 DATABASES = {
      'default': dj_database_url.config()
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
