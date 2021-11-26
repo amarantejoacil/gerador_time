2de17bef79905868d4abc225769b0b6a356e2535
Joacil Amarante <joacil.amarante@gmail.com>
2021-11-26 18:34:04 -0400

    3

--- project/settings.py
+++ project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -84,6 +85,11 @@ DATABASES = {
 """
 
 DATABASES = {
+     'default': dj_database_url.config()
+}
+
+"""
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'geratimenovo',
@@ -93,7 +99,7 @@ DATABASES = {
         'PORT': '5432',
     }
 }
-
+"""
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
